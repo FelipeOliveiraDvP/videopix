6a6b2ca50cd5a7535ed8cea2b2199efb2d903d8b
Felipe de Oliveira Souza <felipe.oliveirac8@gmail.com>
2025-04-27 17:50:58 -0300

    feat: enhance video creation and editing with duration display and video player integration

--- resources/js/Pages/Admin/Videos/Create.tsx
+++ resources/js/Pages/Admin/Videos/Create.tsx
@@ -1,9 +1,13 @@
+import VideoPlayer from "@/Components/Videos/VideoPlayer";
 import AdminLayout from "@/Layouts/AdminLayout";
+import { getDurationInMinutes } from "@/Utils/getDurationInMinutes";
 import { Head, router, useForm } from "@inertiajs/react";
 import {
   ActionIcon,
+  AspectRatio,
   Button,
   Container,
+  Grid,
   Group,
   NumberInput,
   Paper,
@@ -13,6 +17,7 @@ import {
 } from "@mantine/core";
 import { IconArrowLeft } from "@tabler/icons-react";
 import { FormEventHandler } from "react";
+import ReactPlayer from "react-player";
 
 export default function Create() {
   const { data, setData, post, processing, errors, reset } = useForm({
@@ -43,36 +48,63 @@ export default function Create() {
             Novo Vídeo
           </Group>
         </Title>
-        <Container mx={0} size="xs">
+        <Container mx={0} fluid>
           <Stack>
-            <form onSubmit={submit}>
-              <Stack>
-                <TextInput
-                  label="URL do vídeo"
-                  placeholder="https://www.youtube.com/watch?v=XXXXXXXXX"
-                  value={data.url}
-                  onChange={(e) => setData("url", e.currentTarget.value)}
-                  error={errors.url}
-                />
-                <NumberInput
-                  label="Preço"
-                  placeholder="R$ 0,00"
-                  prefix="R$ "
-                  value={data.price}
-                  onChange={(value) => setData("price", value as number)}
-                  error={errors.price}
-                  step={0.01}
-                  hideControls
-                  decimalScale={2}
-                  decimalSeparator=","
-                  thousandSeparator="."
-                  min={0}
-                />
-                <Button type="submit" maw={250} loading={processing}>
-                  Salvar
-                </Button>
-              </Stack>
-            </form>
+            <Grid>
+              <Grid.Col span={{ base: 12, md: 6 }}>
+                <form onSubmit={submit}>
+                  <Stack>
+                    <TextInput
+                      label="URL do vídeo"
+                      placeholder="https://www.youtube.com/watch?v=XXXXXXXXX"
+                      value={data.url}
+                      onChange={(e) => setData("url", e.currentTarget.value)}
+                      error={errors.url}
+                    />
+                    <NumberInput
+                      label="Preço"
+                      placeholder="R$ 0,00"
+                      prefix="R$ "
+                      value={data.price}
+                      onChange={(value) => setData("price", value as number)}
+                      error={errors.price}
+                      step={0.01}
+                      hideControls
+                      decimalScale={2}
+                      decimalSeparator=","
+                      thousandSeparator="."
+                      min={0}
+                    />
+                    <TextInput
+                      label="Duração"
+                      placeholder="https://www.youtube.com/watch?v=XXXXXXXXX"
+                      value={getDurationInMinutes(data.duration)}
+                      onChange={(e) => setData("duration", 0)}
+                      readOnly
+                    />
+                    <Button type="submit" maw={250} loading={processing}>
+                      Salvar
+                    </Button>
+                  </Stack>
+                </form>
+              </Grid.Col>
+              <Grid.Col span={{ base: 12, md: 6 }}>
+                <AspectRatio
+                  ratio={16 / 9}
+                  style={(theme) => ({ borderRadius: theme.radius.md })}
+                >
+                  <ReactPlayer
+                    url={data.url}
+                    width="100%"
+                    height="auto"
+                    controls={false}
+                    onDuration={(duration) => {
+                      setData("duration", duration);
+                    }}
+                  />
+                </AspectRatio>
+              </Grid.Col>
+            </Grid>
           </Stack>
         </Container>
       </Paper>
--- resources/js/Pages/Admin/Videos/Edit.tsx
+++ resources/js/Pages/Admin/Videos/Edit.tsx
@@ -1,10 +1,13 @@
 import AdminLayout from "@/Layouts/AdminLayout";
 import { Video } from "@/types";
+import { getDurationInMinutes } from "@/Utils/getDurationInMinutes";
 import { Head, router, useForm, usePage } from "@inertiajs/react";
 import {
   ActionIcon,
+  AspectRatio,
   Button,
   Container,
+  Grid,
   Group,
   NumberInput,
   Paper,
@@ -14,6 +17,7 @@ import {
 } from "@mantine/core";
 import { IconArrowLeft } from "@tabler/icons-react";
 import { FormEventHandler } from "react";
+import ReactPlayer from "react-player";
 
 export default function Edit() {
   const { video } = usePage<{ video: Video }>().props;
@@ -47,36 +51,63 @@ export default function Edit() {
             Editar Vídeo
           </Group>
         </Title>
-        <Container mx={0} size="xs">
+        <Container mx={0} fluid>
           <Stack>
-            <form onSubmit={submit}>
-              <Stack>
-                <TextInput
-                  label="URL do vídeo"
-                  placeholder="https://www.youtube.com/watch?v=XXXXXXXXX"
-                  value={data.url}
-                  onChange={(e) => setData("url", e.currentTarget.value)}
-                  error={errors.url}
-                />
-                <NumberInput
-                  label="Preço"
-                  placeholder="R$ 0,00"
-                  prefix="R$ "
-                  value={data.price}
-                  onChange={(value) => setData("price", value as number)}
-                  error={errors.price}
-                  step={0.01}
-                  hideControls
-                  decimalScale={2}
-                  decimalSeparator=","
-                  thousandSeparator="."
-                  min={0}
-                />
-                <Button type="submit" maw={250} loading={processing}>
-                  Salvar
-                </Button>
-              </Stack>
-            </form>
+            <Grid>
+              <Grid.Col span={{ base: 12, md: 6 }}>
+                <form onSubmit={submit}>
+                  <Stack>
+                    <TextInput
+                      label="URL do vídeo"
+                      placeholder="https://www.youtube.com/watch?v=XXXXXXXXX"
+                      value={data.url}
+                      onChange={(e) => setData("url", e.currentTarget.value)}
+                      error={errors.url}
+                    />
+                    <NumberInput
+                      label="Preço"
+                      placeholder="R$ 0,00"
+                      prefix="R$ "
+                      value={data.price}
+                      onChange={(value) => setData("price", value as number)}
+                      error={errors.price}
+                      step={0.01}
+                      hideControls
+                      decimalScale={2}
+                      decimalSeparator=","
+                      thousandSeparator="."
+                      min={0}
+                    />
+                    <TextInput
+                      label="Duração"
+                      placeholder="https://www.youtube.com/watch?v=XXXXXXXXX"
+                      value={getDurationInMinutes(data.duration)}
+                      onChange={(e) => setData("duration", 0)}
+                      readOnly
+                    />
+                    <Button type="submit" maw={250} loading={processing}>
+                      Salvar
+                    </Button>
+                  </Stack>
+                </form>
+              </Grid.Col>
+              <Grid.Col span={{ base: 12, md: 6 }}>
+                <AspectRatio
+                  ratio={16 / 9}
+                  style={(theme) => ({ borderRadius: theme.radius.md })}
+                >
+                  <ReactPlayer
+                    url={data.url}
+                    width="100%"
+                    height="auto"
+                    controls={false}
+                    onDuration={(duration) => {
+                      setData("duration", duration);
+                    }}
+                  />
+                </AspectRatio>
+              </Grid.Col>
+            </Grid>
           </Stack>
         </Container>
       </Paper>
