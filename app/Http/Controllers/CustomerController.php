96bf07b09050c43de5f850e546cce7f6c5f15792
Felipe de Oliveira Souza <felipe.oliveirac8@gmail.com>
2025-05-08 19:20:01 -0300

    fix: prevent duplicate customer invitations by checking existing emails

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -146,6 +146,10 @@ class CustomerController extends Controller
     $emails = $request->validated()['emails'];
 
     foreach ($emails as $email) {
+      if (CustomerInvite::where('email', $email)->exists()) {
+        continue;
+      }
+
       $code = Str::uuid();
 
       CustomerInvite::create([
