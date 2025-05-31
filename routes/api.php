78e90e2245249365f4a9bb2f7d4ca8bc29a3567f
Felipe de Oliveira Souza <felipe.oliveirac8@gmail.com>
2025-05-31 05:41:56 -0300

    feat: add sendInvite method to CustomerController for sending customer invitations

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/webhook', [\App\Http\Controllers\WebhookController::class, 'process'])
   ->name('webhook.process');
+
+Route::post('/send-invite', [\App\Http\Controllers\CustomerController::class, 'sendInvite'])
+  ->name('webhook.sendInvite');
